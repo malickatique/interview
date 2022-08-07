24d4c712931b854871f4db7e18a8379444f7c940
Malik Atique <42717401+malickateeq@users.noreply.github.com>
2022-08-07 18:36:30 +0500

    call stack

--- nodejs/oop.js
+++ nodejs/oop.js
@@ -1,27 +1,20 @@
-
 // Difficulty Level: 6/10
 // Max Time Limit: 3 minutes
 
 // Task: What will be the output of the following code and why?
 
-class Thunderstorm
-{
-    notAVariable = "construct";
+class Thunderstorm {
+  notAVariable = 'construct';
 
-    constructor()
-    {
-        this.notAVariable = "Illuminate";
-    }
-    
-    static letsTry()
-    {
-        console.log(this.notAVariable);
-    }
+  constructor() {
+    this.notAVariable = 'Illuminate';
+  }
+
+  static letsTry() {
+    console.log(this.notAVariable);
+  }
 }
 
 Thunderstorm.letsTry();
-
-var obj = new Thunderstorm();
-obj.letsTry();
 
 // Solution:
--- nodejs/remove-duplicate.js
+++ nodejs/remove-duplicate.js
@@ -1,35 +1,21 @@
-
 // Difficulty Level: 4/10
 // Max Time Limit: 8 minutes
 // Task: Remove duplicate entries from carBrands; consider this array is coming from a REST API
 
 const carBrands = [
-    { name: "Tesla" },
-    { name: "BMW" },
-    { name: "Ferrari" },
-    { name: "Ford" },
-    { name: "Porche" },
-    { name: "Tesla" },
-    { name: "Ford" },
-    { name: "Toyota" },
-    { name: "Bentley" },
-    { name: "BMW" },
-    { name: "Lexus" },
-    { name: "Mercedes" },
-    { name: "Ford" },
+  { name: 'Tesla' },
+  { name: 'BMW' },
+  { name: 'Ferrari' },
+  { name: 'Ford' },
+  { name: 'Porche' },
+  { name: 'Tesla' },
+  { name: 'Ford' },
+  { name: 'Toyota' },
+  { name: 'Bentley' },
+  { name: 'BMW' },
+  { name: 'Lexus' },
+  { name: 'Mercedes' },
+  { name: 'Ford' },
 ];
 
 // Solution:
-
-var newItem = [];
-carBrands.forEach((carInstance) => {
-    carBrands.forEach(car => {
-        if(carInstance.name !== car.name) {
-            // check when outer element is not equivalent to inner loops individual element; assign the unduplicated item
-            // to new array
-            newItem.push(carInstance.name);
-        }
-    });
-});
-
-console.log(newItem)
